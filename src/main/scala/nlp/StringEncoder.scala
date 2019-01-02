165696d44d22ef07ccc6bb5ab4c3f8938124b07a
UseK <use.k.james@gmail.com>
2019-01-02 21:44:15 +0900

    Implement StringEncoder.encode(s: String) and decode()

--- src/main/scala/nlp/StringEncoder.scala
+++ src/main/scala/nlp/StringEncoder.scala
@@ -3,18 +3,30 @@ package nlp
 import breeze.linalg.{DenseMatrix, DenseVector}
 
 class StringEncoder(val characters: List[Char]) {
+  val nChars = characters.length
   private def toVectors(m: DenseMatrix[Double]): List[DenseVector[Double]] = {
     (0 until m.rows).map { i =>
       m(i, ::).t
     }.toList
   }
-  private val identityMatrix = DenseMatrix.eye[Double](characters.length)
+  private val identityMatrix = DenseMatrix.eye[Double](nChars)
   private val char2Vector = toVectors(identityMatrix).zipWithIndex.map {t =>
     val (vec, i) = t
     (characters(i), vec)
   }.toMap
   def encode(c: Char): DenseVector[Double] = {
     char2Vector(c)
+  }
+  def encode(s: String): DenseVector[Double] = {
+    s.iterator.map(encode).reduce {(concated, vec) =>
+      DenseVector.vertcat(concated, vec)
+    }
+  }
+  def decode(vec: DenseVector[Double]): String = {
+    vec.toArray.grouped(nChars).toList.map {vec4char =>
+      val maxIndex = vec4char.indexOf(vec4char.max)
+      characters(maxIndex)
+    }.mkString
   }
 }
 
--- src/test/scala/nlp/StringEncoderTest.scala
+++ src/test/scala/nlp/StringEncoderTest.scala
@@ -9,7 +9,23 @@ class StringEncoderTest extends FunSuite {
     val expected = List('a', 'e', 'f', 'i', 'o')
     assert(sVec.characters == expected)
   }
-  test("encode") {
+  test("encode char") {
     assert(sVec.encode('e') == DenseVector(0.0, 1.0, 0.0, 0.0, 0.0))
+  }
+  test("encode string") {
+    assert(sVec.encode("ae") ==
+      DenseVector(
+        1.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 1.0, 0.0, 0.0, 0.0
+      ))
+  }
+  test("decoding") {
+    val vec = DenseVector(
+      1.0, 0.0, 0.0, 0.0, 0.0,
+      0.0, 0.0, 0.0, 1.0, 0.0,
+      0.0, 1.0, 0.0, 0.0, 0.0,
+      0.0, 1.0, 0.0, 0.0, 0.0,
+    )
+    assert(sVec.decode(vec) == "aiee")
   }
 }
