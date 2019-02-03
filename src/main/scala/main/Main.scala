7a030dbd8aea505390915fffb8f1873d8f3b4fbb
UseK <use.k.james@gmail.com>
2019-02-03 18:28:20 +0900

    Snapshot: session.run() returns List() orz

--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -1,23 +1,48 @@
 package main
 import org.platanios.tensorflow.api.tensors.Tensor
 import org.platanios.tensorflow.api.core.Shape
+import org.platanios.tensorflow.api.core.client.{FeedMap, Session}
+import org.platanios.tensorflow.api.ops.variables.ConstantInitializer
 import org.platanios.tensorflow.api.tf
 
-object Main extends App {
-  val t1 = Tensor(1.2, 4.5)
-  val t2 = Tensor(-0.2, 1.1)
-  println(t1)
-  println(t2)
-  println(t1 + t2 == Tensor(1.0, 5.6))
 
-  val inputs      = tf.placeholder[Float](Shape(-1, 10))
-  val outputs     = tf.placeholder[Float](Shape(-1, 10))
-  val predictions = tf.nameScope("Linear") {
-    val weights = tf.variable[Float]("weights", Shape(10, 1), tf.ZerosInitializer)
-    tf.matmul(inputs, weights)
+object Main extends App {
+  TensorFlowExample.sessionRun()
+}
+
+object TensorFlowExample {
+  def sessionRun(): Unit = {
+    val x = tf.placeholder[Float](shape=Shape(1))
+    val w = tf.variable[Float]("w", Shape(1), ConstantInitializer(Tensor(2.0f)))
+    val z = x + w
+    val session = Session()
+    session.run(targets = tf.globalVariablesInitializer())
+    val result = session.run(
+      targets = z,
+      feeds = Map(x -> Tensor(0.6f)))
+    println(result)
   }
-  val loss        = tf.sum(tf.square(predictions - outputs))
-  val optimizer   = tf.train.AdaGrad(1.0f)
-  val trainOp     = optimizer.minimize(loss)
-  println(trainOp)
+
+  def helloTensorFlow = {
+    val t1 = Tensor(1.2, 4.5)
+    val t2 = Tensor(-0.2, 1.1)
+    println(t1)
+    println(t2)
+    println(t1 + t2 == Tensor(1.0, 5.6))
+
+    val inputs = tf.placeholder[Float](Shape(-1, 10))
+    val outputs = tf.placeholder[Float](Shape(-1, 10))
+    val predictions = tf.nameScope("Linear") {
+      val weights = tf.variable[Float](
+        "weights",
+        Shape(10, 1),
+        tf.ZerosInitializer)
+      tf.matmul(inputs, weights)
+    }
+    val loss = tf.sum(tf.square(predictions - outputs))
+    val optimizer = tf.train.AdaGrad(1.0f)
+    val trainOp = optimizer.minimize(loss)
+
+    println(trainOp)
+  }
 }
